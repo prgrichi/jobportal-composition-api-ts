eb6ee39a1ad6560b737967ced9aac6d32afa7f26
Richard Holzner <richardholzner@Richards-MacBook-Pro.local>
2026-01-06 14:49:51 +0100

    Add close btn to BaseModal

--- src/components/modal/BaseModal.vue
+++ src/components/modal/BaseModal.vue
@@ -6,7 +6,7 @@
       <!-- Backdrop (Dark Overlay) -->
       <TransitionChild as="template" enter="ease-out duration-100" enter-from="opacity-0" enter-to="opacity-100"
         leave="ease-in duration-150" leave-from="opacity-100" leave-to="opacity-0">
-        <div class="fixed inset-0 bg-black/80 z-40"></div>
+        <div class="fixed inset-0 bg-black/80 z-40" aria-hidden="true"></div>
       </TransitionChild>
 
       <!-- Modal Container (Centered) -->
@@ -17,19 +17,29 @@
           enter-to="opacity-100 scale-100" leave="ease-in duration-200" leave-from="opacity-100 scale-100"
           leave-to="opacity-0 scale-95">
 
-          <DialogPanel class="bg-background rounded-lg p-6 max-w-md w-full">
+          <DialogPanel :class="sizeClass" class="bg-white relative rounded-lg p-6 w-full max-h-[90vh] overflow-y-auto">
 
             <!-- Modal Title -->
             <DialogTitle class="text-xl font-bold mb-4">
               <slot name="title"></slot>
             </DialogTitle>
 
+            <!-- Close Button -->
+            <button type="button" @click="onClose"
+              class="absolute top-4 right-4 min-w-11 min-h-11 cursor-pointer flex items-center justify-center p-2.5"
+              aria-label="Modal schließen">
+              <Icon name="XMark" type="outline" icon-class="h-7 w-7 lg:h-6 lg:w-6" />
+              <span class="sr-only">Schließen</span>
+            </button>
+
             <!-- Modal Description -->
             <DialogDescription class="text-muted-foreground mb-6">
               <slot name="description"></slot>
             </DialogDescription>
 
-            <slot></slot>
+            <!-- <div class="px-6 overflow-y-auto flex-1"> -->
+            <slot name="content"></slot>
+            <!-- </div> -->
 
             <!-- Action Buttons -->
             <div class="flex gap-4">
@@ -53,6 +63,7 @@ import {
   DialogTitle,
   DialogDescription
 } from '@headlessui/vue';
+import Icon from '@/components/ui/Icon.vue';
 
 export default {
   name: 'BaseModal',
@@ -63,18 +74,37 @@ export default {
     Dialog,
     DialogPanel,
     DialogTitle,
-    DialogDescription
+    DialogDescription,
+    Icon
   },
 
   props: {
     isOpen: {
       type: Boolean,
       required: true
+    },
+    size: {
+      type: String,
+      default: 'md',
+      validator: (value) => ['sm', 'md', 'lg', 'xl', '2xl', '3xl'].includes(value)
     }
   },
 
   emits: ['close'],
 
+  computed: {
+    sizeClass() {
+      const sizes = {
+        sm: 'max-w-sm',
+        md: 'max-w-md',
+        lg: 'max-w-lg',
+        xl: 'max-w-xl',
+        '2xl': 'max-w-2xl',
+        '3xl': 'max-w-3xl'
+      };
+      return sizes[this.size];
+    }
+  },
   methods: {
     // Close modal
     onClose() {
